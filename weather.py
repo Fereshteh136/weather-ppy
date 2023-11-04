4b6391498254d1fdeefa35d71e5591b9b8b24889
Fereshteh136 <149617279+Fereshteh136@users.noreply.github.com>
2023-11-04 13:44:46 +0330

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,9 @@
+is_sunny = True
+is_warm = True
+
+if is_sunny and is_warm:
+    print("It's a sunny and warm day.")
+elif is_sunny or is_warm:
+    print("It's either sunny or warm.")
+else:
+    print("It's neither sunny nor warm.")
